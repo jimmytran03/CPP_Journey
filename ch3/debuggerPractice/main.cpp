4c6d877f64b3a4d827569f820d90d98fd4db5a98
jimmytran03 <jimmy03tran@gmail.com>
2026-06-12 23:00:06 +0100

    Chapter 3 Quiz

--- ch3/debuggerPractice/main.cpp
+++ ch3/debuggerPractice/main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 
+void a()
+{
+	std::cout << "a() called\n";
+}
+
+void b()
+{
+	std::cout << "b() called\n";
+	a();
+}
+
 int main()
 {
-	int x{ 1 };
-	std::cout << x << ' ';
-
-	x = x + 2;
-	std::cout << x << ' ';
-
-	x = x + 3;
-	std::cout << x << ' ';
+	a();
+	b();
 
 	return 0;
 }
