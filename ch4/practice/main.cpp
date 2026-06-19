6956928ed893b0bd14094b72432a258165f880a8
jimmytran03 <jimmy03tran@gmail.com>
2026-06-19 23:33:08 +0100

    ch4

--- ch4/practice/main.cpp
+++ ch4/practice/main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
-#include <cstddef>
+
+// returns true if x and y are equal, false otherwise
+bool isEqual(int x, int y)
+{
+    return x == y; // operator== returns true if x equals y, and false otherwise
+}
 
 int main()
 {
-    int x { 5 };
-    std::size_t s { sizeof(x) };
-    std::cout << s << '\n';
+    std::cout << "Enter an integer: ";
+    int x{};
+    std::cin >> x;
+
+    std::cout << "Enter another integer: ";
+    int y{};
+    std::cin >> y;
+
+    std::cout << std::boolalpha; // print bools as true or false
+
+    std::cout << x << " and " << y << " are equal? ";
+    std::cout << isEqual(x, y) << '\n'; // will return true or false
 
     return 0;
 }
