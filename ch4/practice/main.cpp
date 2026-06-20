ccf9859906f9c50dbc465756eea5b9937d4ce67a
jimmytran03 <jimmy03tran@gmail.com>
2026-06-20 22:21:56 +0100

    ch4

--- ch4/practice/main.cpp
+++ ch4/practice/main.cpp
@@ -1,25 +1,10 @@
 #include <iostream>
 
-// returns true if x and y are equal, false otherwise
-bool isEqual(int x, int y)
-{
-    return x == y; // operator== returns true if x equals y, and false otherwise
-}
-
 int main()
 {
-    std::cout << "Enter an integer: ";
-    int x{};
-    std::cin >> x;
+  std::cout << "\"This is quoted text \"\"n";
+  std::cout << "This string contains a single backslash \\\n";
+  std::cout << "6F in hex is car '\x6F\'n";
 
-    std::cout << "Enter another integer: ";
-    int y{};
-    std::cin >> y;
-
-    std::cout << std::boolalpha; // print bools as true or false
-
-    std::cout << x << " and " << y << " are equal? ";
-    std::cout << isEqual(x, y) << '\n'; // will return true or false
-
-    return 0;
+  return 0;
 }
