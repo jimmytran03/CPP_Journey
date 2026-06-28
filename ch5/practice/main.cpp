efa2cba9fd53a3a30f5407165127f3df6f677bf1
jimmytran03 <jimmy03tran@gmail.com>
2026-06-28 13:34:26 +0100

    ch5

--- ch5/practice/main.cpp
+++ ch5/practice/main.cpp
@@ -1,18 +1,3 @@
-#include <iostream>
-
-void someFcn()
-{
-    #define gravity 9.8
-}
-
-void printGravity(double gravity)
-{
-    std::cout << "gravity: " << gravity << '\n';
-}
-
-int main()
-{
-    printGravity(3.71);
-
-    return 0;
-}
+5 // No suffix
+5L 
+5U
