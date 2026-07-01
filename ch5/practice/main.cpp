9ee8d37bf64b76a71c951c6fab7731f1711a8516
jimmytran03 <jimmy03tran@gmail.com>
2026-07-01 23:18:04 +0100

    ch5 - literals

--- ch5/practice/main.cpp
+++ ch5/practice/main.cpp
@@ -1,3 +1,13 @@
-5 // No suffix
-5L 
-5U
+float f { 4.1f };
+
+double avogadro { 6.02e23 }; 
+
+// magic numbers
+const int maxStudentsPerSchool { numClassrooms * 30 };
+setMax(30);
+
+const int maxStudentsPerClass { 30 };
+const int totalStudents{ numClassrooms * maxStudentsPerClass };
+
+const int maxNameLength { 30 };
+setMax(maxNameLength);
