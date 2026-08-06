119cd1d023ea313c736470729672cf411b4cf791
jimmytran03 <jimmy03tran@gmail.com>
2026-08-06 22:11:10 +0100

    ch7

--- ch7/practice/main.cpp
+++ ch7/practice/main.cpp
@@ -1,18 +1,9 @@
-#include <iostream>
+// Internal global variables definitions:
+static int g_x;          // defines non-initialized internal global variable (zero initialized by default)
+static int g_x{ 1 };     // defines initialized internal global variable
 
-int main()
-{
-    int y{ 5 };
+const int g_y { 2 };     // defines initialized internal global const variable
+constexpr int g_y { 3 }; // defines initialized internal global constexpr variable
 
-    {
-        int x{};
-        std::cin >> x;
-
-        if (x == 4)
-            y = 4;
-    }
-
-    std::cout << y;
-
-    return 0;
-}
+// Internal function definitions:
+static int foo() {};     // defines internal function
