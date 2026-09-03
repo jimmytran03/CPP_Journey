e67004c87c276c990db7615ef9030e3f11dce717
jimmytran03 <jimmy03tran@gmail.com>
2026-09-03 17:38:17 +0100

    ch8

--- ch8/practice/main.cpp
+++ ch8/practice/main.cpp
@@ -1,28 +1,16 @@
 #include <iostream>
 
-void switchExample(int x)
-{
-    switch (x)
-    {
-    case 1:
-        std::cout << "One";
-        return; 
-    case 2:
-        std::cout << "two";
-        return; 
-    case 3:
-        std::cout << "three";
-        return; 
-    default:
-        std::cout << "unknown";
-        return;    
-    }
-}
-
 int main()
 {
-    switchExample(2);
-    std::cout << '\n';
+    for(char c{ 'a' }; c <= 'e'; ++c)
+    {
+        std::cout << c;
+
+        for(int i{ 0 }; i < 3; ++i)
+            std::cout << i;
+
+        std::cout << '\n';
+    }
 
     return 0;
 }
